682e94ba253685754a6557cc2053aaef006b6d24
Joaquín Borges <73504597+JoacinB@users.noreply.github.com>
2023-05-16 11:13:30 -0300

    Update Prueba.java

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -5,6 +5,7 @@ public class Prueba {
 
     public static void main(String[] args) {
         // Archivos para poder hacer el setup inicial del obligatorio 2 e incluir git
+        // Esto imaginate que lo agregas vos
     }
     
 }
